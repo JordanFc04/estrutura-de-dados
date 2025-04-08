6b74cb5d33e95bb66aef9ab510c4d1c8972c891d
JordanFc04 <fernandesjordan32@gmail.com>
2025-04-08 01:57:16 -0300

    Exercício 1: mostrar 6 números na tela

---
+++ vetor.js
@@ -0,0 +1,4 @@
+var numeros = [12, 21, 45, 78, 109, 137];
+for (var i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
+}
---
+++ vetor.ts
@@ -0,0 +1,5 @@
+let numeros: number[] = [12, 21, 45, 78, 109, 137];
+
+for (let i = 0; i < numeros.length; i++) {
+console.log(numeros[i]);
+}
